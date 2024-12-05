c58c403c515cf5fcc42a7614f9f53dee5ed2297a
alesstongwen <alesstongwen@gmail.com>
2024-12-05 10:45:52 -0800

    get tanstack working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
-// import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import MainBoard from "./MainBoard.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
@@ -17,8 +18,12 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <MainBoard />
-  // </StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
