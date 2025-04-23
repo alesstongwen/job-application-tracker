70fe9a0ff53aaab57e2cf3ccb871a726b1304c61
alesstongwen <alesstongwen@gmail.com>
2025-04-23 14:19:36 -0700

    update app.ts

--- server/app.ts
+++ server/app.ts
@@ -7,25 +7,19 @@ import { cors } from 'hono/cors';
 
 const app = new Hono();
 
-// Middleware
 app.use('*', logger());
 
-// For production, allow your Render domain
 app.use('*', cors({ 
-  origin: ["http://localhost:5173", "https://your-app-name.onrender.com"], 
+  origin: ["http://localhost:5173", "https://job-application-tracker-4r9c.onrender.com/"], 
   credentials: true 
 }));
 
-// API Routes
+app.use('/assets/*', serveStatic({ root: './frontend/dist' }));
+app.use('/favicon.ico', serveStatic({ root: './frontend/dist' }));
+
 app.route('/api/dashboard', dashboardsRoute);
 app.route('/auth', authRoute);
 
-// Serve static assets (JS, CSS, images, etc.)
-app.use('/static/*', serveStatic({ root: './frontend/dist' }));
-
-// Serve all other routes (SPA fallback)
 app.use('*', serveStatic({ path: './frontend/dist/index.html' }));
 
 export default app;
-
-export type apiRoute = typeof app;
